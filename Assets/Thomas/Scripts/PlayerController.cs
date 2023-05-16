4a5e5c41da67810b9a967c0fd39ef267eab0124a
oD Wreckz <thomascain7i@gmail.com>
2023-05-16 16:25:02 +0100

    Player Controller

--- Assets/Thomas/Scripts/PlayerController.cs
+++ Assets/Thomas/Scripts/PlayerController.cs
@@ -3,47 +3,50 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float moveSpeed = 5f;
-    public float rotationSpeed = 10f;
     public float jumpForce = 5f;
     public int maxJumps = 2;
+    private int jumpsRemaining;
 
     private Rigidbody rb;
-    private int jumpsLeft;
+    private bool isGrounded;
 
-    private void Start()
+    void Start()
     {
         rb = GetComponent<Rigidbody>();
-        rb.freezeRotation = true;
-        jumpsLeft = maxJumps;
+        jumpsRemaining = maxJumps;
     }
 
-    private void FixedUpdate()
+    void Update()
     {
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
 
-        // Calculate movement and rotation vectors
-        Vector3 movement = new Vector3(moveHorizontal, 0f, moveVertical) * moveSpeed * Time.fixedDeltaTime;
-        Quaternion rotation = Quaternion.Euler(0f, moveHorizontal * rotationSpeed * Time.fixedDeltaTime, 0f);
+        Vector3 movement = new Vector3(moveHorizontal, 0f, moveVertical) * moveSpeed;
+        rb.velocity = new Vector3(movement.x, rb.velocity.y, movement.z);
 
-        // Apply movement and rotation to the rigidbody
-        rb.MovePosition(transform.position + movement);
-        rb.MoveRotation(rb.rotation * rotation);
-
-        if (Input.GetButtonDown("Jump") && jumpsLeft > 0)
+        if (Input.GetButtonDown("Jump") && (isGrounded || jumpsRemaining > 0))
         {
-            // Jump
-            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
-            jumpsLeft--;
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.VelocityChange);
+            isGrounded = false;
+            jumpsRemaining--;
+        }
+
+        if (moveHorizontal < 0)
+        {
+            transform.rotation = Quaternion.Euler(0f, 180f, 0f);
+        }
+        else if (moveHorizontal > 0)
+        {
+            transform.rotation = Quaternion.Euler(0f, 0f, 0f);
         }
     }
 
-    private void OnCollisionEnter(Collision collision)
+    void OnCollisionEnter(Collision collision)
     {
-        // Reset jumps if player lands on the ground
         if (collision.gameObject.CompareTag("Ground"))
         {
-            jumpsLeft = maxJumps;
+            isGrounded = true;
+            jumpsRemaining = maxJumps;
         }
     }
 }
