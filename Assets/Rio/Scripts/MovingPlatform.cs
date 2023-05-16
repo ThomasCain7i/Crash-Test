e3e5cf05091e34fdd2a46d4e5141c9b8d79c0c3c
RioFrancisAikman <115077816+RioFrancisAikman@users.noreply.github.com>
2023-05-16 16:18:16 +0100

    Made 2 types of moving platforms
    MovingLR for left and right, MovingFB for forwards and backwards

--- Assets/Rio/Scripts/MovingPlatform.cs
+++ Assets/Rio/Scripts/MovingPlatform.cs
@@ -6,6 +6,12 @@ public class MovingPlatform : MonoBehaviour
 {
     public float moveTimer;
 
+    //Moving left and right
+    public bool isMovingLR;
+
+    //Moving forwards and backwards
+    public bool isMovingFB;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -17,22 +23,46 @@ public class MovingPlatform : MonoBehaviour
     {
         moveTimer += Time.deltaTime;
 
-        if (moveTimer <= 5.0f)
+        if (isMovingLR)
         {
+            if (moveTimer <= 5.0f)
+            {
             //Platform moves right
             transform.Translate(new Vector3(1 * Time.deltaTime, 0, 0));
 
-        }
-        else if (moveTimer <= 10.0f)
-        {
+            }
+            else if (moveTimer <= 10.0f)
+            {
             //Platform moves left
             transform.Translate(new Vector3(-1 * Time.deltaTime, 0, 0));
 
-        }
-        else if (moveTimer >= 10.0f)
-        {
+            }
+            else if (moveTimer >= 10.0f)
+            {
             // resets timer
             moveTimer = 0;
+            }
+        }
+
+        if (isMovingFB)
+        {
+            if (moveTimer <= 5.0f)
+            {
+            //Platform moves forwards
+            transform.Translate(new Vector3(0, 0, 1 * Time.deltaTime));
+
+            }
+            else if (moveTimer <= 10.0f)
+            {
+            //Platform moves backwards
+            transform.Translate(new Vector3(0, 0, -1 * Time.deltaTime));
+
+            }
+            else if (moveTimer >= 10.0f)
+            {
+            // resets timer
+            moveTimer = 0;
+            }
         }
     }
 }
