3e0825378a7bbb402a27e5f40a589e93e2de6a1d
oD Wreckz <thomascain7i@gmail.com>
2023-05-23 16:42:20 +0100

    Prefabs and rename

--- Assets/Thomas/Scripts/NextLevel/GoldNextLevel.cs
+++ Assets/Thomas/Scripts/NextLevel/GoldNextLevel.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class NextLevel : MonoBehaviour
+public class GoldNextLevel : MonoBehaviour
 {
     public PlayerController playerController;
 
