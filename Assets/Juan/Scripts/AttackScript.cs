1c106107c58fdf717b9d00272b5bc7515062d5ea
Juan Sandoval <114575778+Juampa97@users.noreply.github.com>
2023-05-23 12:44:35 +0100

    Smash attack basics added
    I have started working on it, the basic movement of smashing down while jumping has been added, I will start working on the knock back force for objects to be affected by the smash

--- Assets/Juan/Scripts/AttackScript.cs
+++ Assets/Juan/Scripts/AttackScript.cs
@@ -4,17 +4,27 @@ using UnityEngine;
 
 public class AttackScript : MonoBehaviour
 {
+    //BARK STUFF (POINT OF SPAWN, SPEED ETC..)
     public Transform barkSpawn;
     public GameObject barkBullet;
     public float barkSpeed = 5f;
 
+    //COOLDOWN
     public float coolDownTime;
     public float nextBarkTime;
+
+    // Rigidbody
+    private Rigidbody rb;
+    private bool isGrounded;
+
+    //SMASH
+    public float smashForce; 
+    
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
@@ -22,18 +32,30 @@ public class AttackScript : MonoBehaviour
     {
         if(Time.time > nextBarkTime)
         {
+            //BARK ATTACK - JUAN
             if (Input.GetKeyDown(KeyCode.R))
             {
-                var ability = Instantiate(barkBullet, barkSpawn.position, barkSpawn.rotation);
-                ability.GetComponent<Rigidbody>().velocity = barkSpawn.forward * barkSpeed;
+                var ability = Instantiate(barkBullet, barkSpawn.position, barkSpawn.rotation); //SPAWNS THE ATTACK FROM THE SPECIFIC POINT OFF THE PLAYER
+                ability.GetComponent<Rigidbody>().velocity = barkSpawn.forward * barkSpeed; //DEALS WITH THE FORCE AND SPEED OF THE BARK
 
 
-                //COOLDOWN FOR THE ABILITY 
+                //COOLDOWN FOR THE BARK - JUAN
                 nextBarkTime = Time.time + coolDownTime;
             }
         }
        
+        //SMASH ATTACK - JUAN
+
+        if(isGrounded == false)
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                rb.AddForce(Vector3.down * smashForce, ForceMode.VelocityChange);
+            }
+        }
     }
+               
+
 
     
 }
