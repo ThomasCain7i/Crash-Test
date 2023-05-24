c4e173239c4f5cd4ecab43e48eb60949ffe36675
Camron Obeng <101772uk@saeinstitute.edu>
2023-05-24 14:24:33 +0100

    scene update

---
+++ Assets/Camron/Enemies/SelfDestructEnemy.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DistanceScript : MonoBehaviour
+{
+    [SerializeField] GameObject player;
+    float stopDist = 100f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        float distance = CalcDistBetweenVector3(player.transform.position, transform.position);
+        //Debug.Log("player is " + distance + "away from us");
+
+        transform.LookAt(player.transform.position);
+
+        if (distance > stopDist)
+        {
+            transform.Translate(Vector3.forward * Time.deltaTime);
+        }
+    }
+
+    public float CalcDistBetweenVector3(Vector3 ObjectA, Vector3 ObjectB)
+    {
+        float x = ObjectA.x + ObjectB.x;
+        float y = ObjectA.x + ObjectB.y;
+        float z = ObjectA.x + ObjectB.z;
+
+        x = Mathf.Pow(x, 2);
+        y = Mathf.Pow(x, 2);
+        x = Mathf.Pow(x, 2);
+
+        float sqXYZ = x + y + z;
+
+        float calcDist = Mathf.Sqrt(sqXYZ);
+
+        return calcDist;
+
+    }
+}
---
+++ Assets/Camron/MovingPlatArrow.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlatArrow : MonoBehaviour
+{
+    public float moveTimer;
+
+    //Moving left and right
+    public bool isMovingLR;
+
+    //Moving forwards and backwards
+    public bool isMovingFB;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moveTimer += Time.deltaTime;
+
+        if (isMovingLR)
+        {
+            if (moveTimer <= 5.0f)
+            {
+            //Platform moves left
+            transform.Translate(new Vector3(-5 * Time.deltaTime , 0, 0));
+
+            }
+            else if (moveTimer <= 10.0f)
+            {
+            //Platform moves right
+            transform.Translate(new Vector3(5 * Time.deltaTime, 0, 0));
+
+            }
+            else if (moveTimer >= 10.0f)
+            {
+            // resets timer
+            moveTimer = 0;
+            }
+        }
+
+        if (isMovingFB)
+        {
+            if (moveTimer <= 5.0f)
+            {
+            //Platform moves forwards
+            transform.Translate(new Vector3(0, 0, 2 * Time.deltaTime));
+
+            }
+            else if (moveTimer <= 10.0f)
+            {
+            //Platform moves backwards
+            transform.Translate(new Vector3(0, 0, -2 * Time.deltaTime));
+
+            }
+            else if (moveTimer >= 10.0f)
+            {
+            // resets timer
+            moveTimer = 0;
+            }
+        }
+    }
+}
