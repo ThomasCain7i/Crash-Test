38ca55785488cada7aa05000fbf5a00e42acb976
Thomas Cain <115077729+ThomasCain7i@users.noreply.github.com>
2023-06-16 15:40:17 +0100

    Knockback added to smash

--- Assets/Thomas/Scripts/Player/Attack/SmashProjectile.cs
+++ Assets/Thomas/Scripts/Player/Attack/SmashProjectile.cs
@@ -5,6 +5,7 @@ public class SmashProjectile : MonoBehaviour
     public float damage;
     public float destroyTimer = .5f;
     public PlayerController playerController;
+    public float knockbackForce = 10f; // Adjust this value to control the strength of the knockback force
 
     // Start is called before the first frame update
     void Start()
@@ -17,7 +18,7 @@ public class SmashProjectile : MonoBehaviour
     {
         destroyTimer -= Time.deltaTime;
 
-        if ( destroyTimer < 0 )
+        if (destroyTimer < 0)
         {
             Destroy(gameObject);
         }
@@ -28,9 +29,18 @@ public class SmashProjectile : MonoBehaviour
         //On collision with bullet do:
         switch (other.gameObject.tag)
         {
-            // Collision with enemy deal 1 damage and destory
+            // Collision with enemy deal damage and apply knockback
             case "Enemy":
-                other.gameObject.GetComponent<EnemyHealth>().TakeDamage(damage);
+                EnemyHealth enemyHealth = other.gameObject.GetComponent<EnemyHealth>();
+                enemyHealth.TakeDamage(damage);
+
+                Rigidbody enemyRigidbody = other.gameObject.GetComponent<Rigidbody>();
+                if (enemyRigidbody != null)
+                {
+                    Vector3 knockbackDirection = other.transform.position - transform.position;
+                    knockbackDirection.Normalize();
+                    enemyRigidbody.AddForce(knockbackDirection * knockbackForce, ForceMode.Impulse);
+                }
                 break;
         }
     }
