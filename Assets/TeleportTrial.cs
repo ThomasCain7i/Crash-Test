a62752b43737495dcda9f74594c68f00eadf45b0
Juan Sandoval <114575778+Juampa97@users.noreply.github.com>
2023-07-17 20:26:23 +0100

    2D switch camera added to water level
    I have added a way to switch cameras and create a 2D look for part of my level

---
+++ Assets/TeleportTrial.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeleportTrial : MonoBehaviour
+{
+    
+    
+    public GameObject mainCamera;
+    public GameObject secondCamera; 
+
+    public Transform player;
+    public Transform otherEnd;
+    public Transform camera3D;
+    public Transform camera2;
+
+    public Vector3 threeOffset;
+    public float smoothSpeed = 0.125f;
+    private bool mode2D; 
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (mode2D == true)
+        {
+            secondCamera.SetActive(true);
+            mainCamera.SetActive(false);
+            
+
+
+
+
+        }
+        else if(mode2D == false)
+        {
+            secondCamera.SetActive(false);
+            mainCamera.SetActive(true);
+        }
+    }
+
+   
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            mode2D = true;
+            
+        } 
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            mode2D = false;
+
+        }
+
+    }
+
+
+
+}
