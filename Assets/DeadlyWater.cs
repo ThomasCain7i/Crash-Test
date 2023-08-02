5de519a415117fdbcfeb67a390ed0981cda21831
Thomas Cain <115077729+ThomasCain7i@users.noreply.github.com>
2023-08-02 18:52:24 +0100

    Deadly Water

---
+++ Assets/DeadlyWater.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeadlyWater : MonoBehaviour
+{
+    public PlayerController playerController;
+
+    [SerializeField]
+    private float damage = 10;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerController = FindObjectOfType<PlayerController>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            playerController.TakeDamage(damage);
+        }
+    }
+}
