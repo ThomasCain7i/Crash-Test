8104fcbae91cc4286e9ca256c7aafc11daa304b4
Thomas Cain <115077729+ThomasCain7i@users.noreply.github.com>
2023-08-05 18:37:43 +0100

    Tutorial Finished

--- Assets/Thomas/Scripts/Enemy/Statistics/EnemyHealth.cs
+++ Assets/Thomas/Scripts/Enemy/Statistics/EnemyHealth.cs
@@ -4,11 +4,14 @@ public class EnemyHealth : MonoBehaviour
 {
     public float currentHealth; // The current health of the enemy
     public float maxHealth; // The maximum health of the enemy
+    [SerializeField]
+    private TutorialWall tutorialWall;
 
     // Start is called before the first frame update
     void Start()
     {
         currentHealth = maxHealth; // Initialize the current health to the maximum health
+        tutorialWall = GetComponent<TutorialWall>();
     }
 
     public void TakeDamage(float amount)
@@ -17,6 +20,11 @@ public class EnemyHealth : MonoBehaviour
 
         if (currentHealth <= 0)
         {
+            if(tutorialWall != null)
+            {
+                tutorialWall.DeleteWalls();
+            }
+
             Destroy(gameObject); // If the current health drops to or below zero, destroy the enemy object
         }
     }
--- Assets/TutorialWall.cs
+++ Assets/TutorialWall.cs
@@ -4,30 +4,14 @@ using UnityEngine;
 
 public class TutorialWall : MonoBehaviour
 {
-    private EnemyHealth enemyHealth;
-
     [SerializeField]
     private GameObject wall1, wall2;
 
-    [SerializeField]
-    private float health;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        enemyHealth = GetComponent<EnemyHealth>();
-    }
-
     // Update is called once per frame
-    void Update()
+    public void DeleteWalls()
     {
-        health = enemyHealth.currentHealth;
-
-        if (health <= 0)
-        {
             Destroy(wall1);
             Destroy(wall2);
             Debug.Log("Destory Walls");
-        }
     }
 }
